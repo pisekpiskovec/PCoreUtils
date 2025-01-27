57e3a776d81f7fc48726f776692b698ea55309b2
Písek Pískovec <77384152+pisekpiskovec@users.noreply.github.com>
2025-01-27 11:53:18 +0100

    echo command

--- src/echo/src/main.rs
+++ src/echo/src/main.rs
@@ -1,3 +1,19 @@
+use clap::Parser;
+
+#[derive(Parser, Debug)]
+#[command(version, long_about = None)]
+struct Command {
+    input: Option<String>,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args = Command::parse();
+    println!(
+        "{}",
+        if let Some(get_input) = args.input.as_deref() {
+            String::from(get_input)
+        } else {
+            String::new()
+        }
+    );
 }
